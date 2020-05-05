0f41176944dd738a5fccecce28d3f8a610da6960
Jordan Place <tapman90@gmail.com>
2020-05-05 09:57:03 -0700

    tailwind working

---
+++ styles.11ty.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const postcss = require("postcss");
+
+module.exports = class {
+  async data() {
+    const rawFilepath = path.join(__dirname, "_includes/css/styles.css");
+    return {
+      permalink: `css/styles.css`,
+      rawFilepath,
+      rawCss: await fs.readFileSync(rawFilepath),
+    };
+  }
+
+  async render({ rawCss, rawFilepath }) {
+    return await postcss([require("tailwindcss"), require("autoprefixer")])
+      .process(rawCss, { from: rawFilepath })
+      .then((result) => result.css);
+  }
+};
