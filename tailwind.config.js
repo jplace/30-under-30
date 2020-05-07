e77ece38f8747f69e03966cb144ef39862029aa5
Jordan Place <tapman90@gmail.com>
2020-05-07 09:54:28 -0700

    more layout

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "dusky-blue": "#475f94",
+        "loverless-gold": "#fddc5c",
+        "orangey-red": "#fa4224",
+      },
+    },
+  },
+};
