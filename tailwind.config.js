68cadb7f6f21325faf853d4f10b857180bdb5429
Jordan Place <tapman90@gmail.com>
2020-05-07 13:21:02 -0700

    more layout stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,13 @@ module.exports = {
       colors: {
         "dusky-blue": "#475f94",
         "loverless-gold": "#fddc5c",
-        "orangey-red": "#fa4224",
+        "orangey-red": { default: "#fa4224", dark: "#d62204" },
+        "dreary-red": "#2b0000",
       },
+    },
+    fontFamily: {
+      display: ["Crimson Text", "serif"],
+      body: ["Crimson Text", "serif"],
     },
   },
 };
