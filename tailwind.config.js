f9957e8f7c65d769646a67c1ad335c444526b9b0
Jordan Place <tapman90@gmail.com>
2020-05-07 18:04:03 -0700

    in the middle of the progress bar and choosing better colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
         "loverless-gold": "#fddc5c",
         "orangey-red": { default: "#fa4224", dark: "#d62204" },
         "dreary-red": "#2b0000",
+        "header-blue": {
+          100: "#c8dcff",
+          200: "#b6caff",
+          300: "#7c91ca",
+          400: "#7d89e7",
+          500: "#0b3161",
+        },
+        concrete: "#F5F9FF",
       },
     },
     fontFamily: {
